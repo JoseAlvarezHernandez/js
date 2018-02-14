0a01e1ac2a94d397dcd97e7c04f868470d2c4561
Jose de Jesus Alvarez Hernandez <alvarez_3993@hotmail.com>
2018-02-14 17:43:35 -0600

    Update getDates.js

--- getDates.js
+++ getDates.js
@@ -14,11 +14,17 @@ const getDates = (start, end) => {
 }
 
 Date.prototype.toFormat = () => {
-    let month = this.getMonth() + 1,
-        year = this.getFullYear(),
+    let year = this.getFullYear(),
+        month = this.getMonth() + 1,
         day = this.getDate(),
         hours = this.getHours(),
-        minutes = this.getMinutes(),
+        minutes = this.getMinutes() ,
         seconds = this.getSeconds();
-    return (`${year}-${(month > 9 ? month : '0' + month)}-${(day > 9 ? day : '0' + day)} ${(hours > 9 ? hours : `0${hours}`)}:${(minutes > 9 ? minutes : `0${minutes}`)}:${(seconds > 9 ? seconds : `0${seconds}`)}`);
+    month = month > 9 ? month: `0${month}`;
+    day = day > 9 ? day: `0${day}`;
+    hours = hours > 9 ? hours: `0${hours}`;
+    minutes = minutes > 9 ? minutes: `0${minutes}`;
+    seconds = seconds > 9 ? seconds: `0${seconds}`;
+    
+    return (`${year}-${month}-${day} ${hours}:${minutes}:${seconds}`);
 };
